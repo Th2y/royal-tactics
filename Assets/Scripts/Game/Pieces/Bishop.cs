0382250928bf0c5f3b35a50d20714cff3f3a3699
thay.santos-at-538590928221 <thayanecarvalho3@gmail.com>
2026-02-15 23:31:31 -0300

    Core logic of bishop, knight, rook and queen

--- Assets/Scripts/Game/Pieces/Bishop.cs
+++ Assets/Scripts/Game/Pieces/Bishop.cs
@@ -1,15 +1,83 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Bishop : Piece
 {
-    public override List<Tile> GetValidCaptures(BoardController board)
+    private static readonly Vector2Int[] directions =
     {
-        throw new System.NotImplementedException();
-    }
+        new(1, 1),
+        new(1, -1),
+        new(-1, -1),
+        new(-1, 1)
+    };
 
     public override List<Tile> GetValidMoves(BoardController board)
     {
-        throw new System.NotImplementedException();
+        List<Tile> moves = new();
+
+        int startX = (int)currentTile.pos.x;
+        int startY = (int)currentTile.pos.y;
+
+        foreach (var dir in directions)
+        {
+            int x = startX + dir.x;
+            int y = startY + dir.y;
+
+            while (true)
+            {
+                Tile tile = board.GetTile(x, y);
+
+                if (tile == null)
+                    break;
+
+                if (tile.IsOccupied)
+                    break;
+
+                moves.Add(tile);
+
+                x += dir.x;
+                y += dir.y;
+            }
+        }
+
+        return moves;
+    }
+
+    public override List<Tile> GetValidCaptures(BoardController board)
+    {
+        List<Tile> captures = new();
+
+        int startX = (int)currentTile.pos.x;
+        int startY = (int)currentTile.pos.y;
+
+        foreach (var dir in directions)
+        {
+            int x = startX + dir.x;
+            int y = startY + dir.y;
+
+            while (true)
+            {
+                Tile tile = board.GetTile(x, y);
+
+                if (tile == null)
+                    break;
+
+                if (!tile.IsOccupied)
+                {
+                    x += dir.x;
+                    y += dir.y;
+                    continue;
+                }
+
+                if (tile.piece.isFromPlayer != isFromPlayer)
+                {
+                    captures.Add(tile);
+                }
+
+                break;
+            }
+        }
+
+        return captures;
     }
 }
--- Assets/Scripts/Game/Pieces/Knight.cs
+++ Assets/Scripts/Game/Pieces/Knight.cs
@@ -1,15 +1,66 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Knight : Piece
 {
-    public override List<Tile> GetValidCaptures(BoardController board)
+    private static readonly Vector2Int[] directions =
     {
-        throw new System.NotImplementedException();
-    }
+        new(1, 2),
+        new(2, 1),
+        new(2, -1),
+        new(1, -2),
+        new(-1, -2),
+        new(-2, -1),
+        new(-2, 1),
+        new(-1, 2)
+    };
 
     public override List<Tile> GetValidMoves(BoardController board)
     {
-        throw new System.NotImplementedException();
+        List<Tile> moves = new();
+
+        int x = (int)currentTile.pos.x;
+        int y = (int)currentTile.pos.y;
+
+        foreach (var dir in directions)
+        {
+            Tile target = board.GetTile(x + dir.x, y + dir.y);
+
+            if (target == null)
+                continue;
+
+            if (!target.IsOccupied)
+            {
+                moves.Add(target);
+            }
+        }
+
+        return moves;
+    }
+
+    public override List<Tile> GetValidCaptures(BoardController board)
+    {
+        List<Tile> captures = new();
+
+        int x = (int)currentTile.pos.x;
+        int y = (int)currentTile.pos.y;
+
+        foreach (var dir in directions)
+        {
+            Tile target = board.GetTile(x + dir.x, y + dir.y);
+
+            if (target == null)
+                continue;
+
+            if (!target.IsOccupied)
+                continue;
+
+            if (target.piece.isFromPlayer != isFromPlayer)
+            {
+                captures.Add(target);
+            }
+        }
+
+        return captures;
     }
 }
