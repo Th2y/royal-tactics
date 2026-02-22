d30ad11839420251416caab0c9dc61187a734147
thay.santos-at-538590928221 <thayanecarvalho3@gmail.com>
2026-02-22 00:20:17 -0300

    Preparing assets for Save and Attack Game Modes

---
+++ Assets/Scripts/Game/Mode/AttackTheKing/AttackTheKingModeUI.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTheKingModeUI : GameModeUIBase
+{
+    public override void BuildUI()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RefreshButtons()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetOptions(List<PieceDefinitionSO> options)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetOptions(List<TileName> options)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Game/Mode/SaveTheKing/SaveTheKingMode.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AttackTheKing : GameModeBase
+{
+    public override void PlayerFinishedMoves()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetGameTurn(GameTurn newPhase)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool CheckAIWin()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool CheckPlayerWin()
+    {
+        throw new System.NotImplementedException();
+    }
+}
